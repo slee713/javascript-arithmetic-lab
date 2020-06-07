14bb992edd246ef3c1c1041d0c67875cf1c0085b
Steven Lee <steven.lee1393@gmail.com>
2020-06-07 13:47:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,50 @@
+
+function add(){
+  return 1+2
+}
+
+function subtract(){
+  return 5-4
+}
+
+function multiply(){
+  return 4*2
+}
+
+function divide(){
+  return 10/5
+}
+
+function add(a,b){
+  return a+b
+}
+
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+
+function divide(a,b){
+  return a/b
+}
+
+function increment(n){
+  n++
+  return n
+}
+
+function decrement(n){
+  n--
+  return n
+}
+
+function makeInt(n){
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+}
